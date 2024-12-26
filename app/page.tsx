d9b76072e0b3e80ef1a143416f7215ca6aaf5150
Amandeep-rawat <ar0466467@gmail.com>
2024-12-26 15:04:16 +0530

    new

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ const Footer = dynamic(() => import('@/components/Footer'),{ loading: () => <p>L
 export default function Home() {
   return (
   <main className="relative flex justify-center items-center flex-col overflow-clip mx-auto sm:px-10 px-5 bg-black-100">
-    {/* <div className='fixed flex flex-col justify-center items-center z-[1000] top-2 right-2'>
+    <div className='fixed flex flex-col justify-center items-center z-[1000] top-2 right-2'>
         <Link target="_blank" href="https://www.linkedin.com/in/amandeep-rawat-1622a82ba/">
             <button className="relative inline-flex h-14 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
       <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
@@ -35,7 +35,7 @@ export default function Home() {
     </button>
         </Link>
     <h3 className="font-bold">R<span className="text-[#be2ed6] ">W</span>T</h3>
-    </div> */}
+    </div>
 
     <div className="max-w-7xl w-full">
       <FloatingNav navItems={navItems}/>
